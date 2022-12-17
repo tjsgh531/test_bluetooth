c43c361e3977a7112a24e38be794075c204115b4
Sun Ho <tjsgh5768@gmail.cmo>
2022-12-17 11:21:44 +0900

    bluetooth가 하나씩 프린트 되어야함

--- bluetooth5/ViewController.swift
+++ bluetooth5/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var aaa: UILabel!
     var centralManager : CBCentralManager!
+    var targetPeripheral: CBPeripheral!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-    
-
 
 }
 
@@ -46,11 +45,32 @@ extension ViewController: CBCentralManagerDelegate{
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         print(peripheral)
+        aaa.text = peripheral as! String
+        
+        // 여기에 리스트 넣는 코드 넣어줘야함
+        // 리스트에서 해당item 선택시 연결하는 코드로 ㄲ
+        
+        //블루투스 uuidString 이 "identifier" 이면 걔랑 연결하는 코드
+        if peripheral.identifier.uuidString == "identifier" {
+            self.targetPeripheral = peripheral
+            targetPeripheral.delegate = self
+            centralManager.stopScan() // 스캔 멈출고
+            centralManager.connect(targetPeripheral) //걔랑 연결해라
+        }
     }
     
-//
-//    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData:[String : Any], rssi RSSI: NSNumber) {
-//        print(peripheral)
-//        aaa.text = peripheral as! String
-//    }
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected!!")
+        targetPeripheral.discoverServices(nil)
+    }
+}
+
+extension ViewController: CBPeripheralDelegate{
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = targetPeripheral.services else {return}
+        for service in services {
+            print(service)
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
 }
